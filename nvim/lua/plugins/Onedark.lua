c43988687d7e6bbf8ef931cd148040bcb04e0df7
Theyashsawarkar <yashsawarkar01@gmail.com>
2024-03-14 01:51:18 +0530

    tmux and nvim setup

---
+++ nvim/lua/plugins/Onedark.lua
@@ -0,0 +1,16 @@
+--NOTE:-
+--      uncomment the below lines in function to make onedark default theme
+--      but then transparent_background wont work
+
+return {
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        transparent = true, -- Making it transparent
+      })
+      vim.cmd("colorscheme onedark") -- Setting onedark as the colorscheme
+    end,
+    priority = 100, -- Set a high priority for this plugin
+  },
+}
